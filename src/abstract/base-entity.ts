365a4efc96c328227fd3b29b4a945b9e02b0f04f
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-03-21 05:04:33 +0700

    rename some kolom

--- src/abstract/base-entity.ts
+++ src/abstract/base-entity.ts
@@ -1,11 +1,14 @@
 import { BeforeInsert, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class BaseEntity {
-  @CreateDateColumn()
+  @CreateDateColumn({
+    name: 'created_at',
+  })
   createdAt: Date;
 
   @UpdateDateColumn({
     nullable: true,
+    name: 'updated_at',
   })
   updatedAt: Date | null;
 
